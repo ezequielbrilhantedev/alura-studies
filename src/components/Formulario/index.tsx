6bf008074198e98e60c40c43128684759c9978fc
Ezequiel Brilhante <ezequielrb7@gmail.com>
2022-06-15 16:47:57 -0300

    add useState in application

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,34 +1,26 @@
-import Botao from "../Botao";
-import style from "./Formulario.module.scss";
+import { useState } from 'react'
+import Botao from '../Botao'
+import style from './Formulario.module.scss'
 
 function Formulario() {
+  const [estudo, setEstudo] = useState({
+    tarefa: '',
+    tempo: '',
+  })
+
   return (
     <form className={style.novaTarefa}>
       <div className={style.inputContainer}>
         <label htmlFor="tarefa">Adicione um novo estudo</label>
-        <input
-          type="text"
-          name="tarefa"
-          id="tarefa"
-          placeholder="O que você quer estudar?"
-          required
-        />
+        <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar?" required />
       </div>
       <div className={style.inputContainer}>
         <label htmlFor="">Tempo</label>
-        <input
-          type="time"
-          step="1"
-          name="tempo"
-          id="tempo"
-          min="00:00:00"
-          max="01:30:00"
-          required
-        />
+        <input type="time" step="1" name="tempo" value={estudo.tempo} id="tempo" min="00:00:00" max="01:30:00" required />
       </div>
       <Botao texto="Adicionar" />
     </form>
-  );
+  )
 }
 
-export default Formulario;
+export default Formulario
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,31 +1,38 @@
-import Item from "./Item";
-import style from "./Lista.module.scss";
+import { useState } from 'react'
+import Item from './Item'
+import style from './Lista.module.scss'
 
 function Lista() {
-  const tarefas = [
+  const [tarefas, setTarefas] = useState([
     {
-      tarefa: "React",
-      tempo: "02:00:00",
+      tarefa: 'React',
+      tempo: '02:00:00',
     },
     {
-      tarefa: "Javascript",
-      tempo: "01:00:00",
+      tarefa: 'Javascript',
+      tempo: '01:00:00',
     },
     {
-      tarefa: "Typescript",
-      tempo: "03:00:00",
+      tarefa: 'Typescript',
+      tempo: '03:00:00',
     },
-  ];
+  ])
   return (
     <aside className={style.listaTarefas}>
-      <h2>Estudos do dia</h2>
+      <h2
+        onClick={() => {
+          setTarefas([...tarefas, { tarefa: 'Estudar estado', tempo: '05:00:00' }])
+        }}
+      >
+        Estudos do dia
+      </h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item key={index} {...item} />
         ))}
       </ul>
     </aside>
-  );
+  )
 }
 
-export default Lista;
+export default Lista
