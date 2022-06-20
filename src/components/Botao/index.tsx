5ab36dcb4e86ab5223771697b7aa6fad6abeeaaa
Ezequiel Brilhante <e.ribeiro.brilhante@accenture.com>
2022-06-20 15:58:29 -0300

    created functionality stopwacth

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import style from './Botao.module.scss'
+import React from "react";
+import style from "./Botao.module.scss";
 
 class Botao extends React.Component<{
-  type?: 'button' | 'submit' | 'reset' | undefined
-  texto: string
+  type?: "button" | "submit" | "reset" | undefined;
+  onClick?: (evt: any) => void;
+  texto: string;
 }> {
   render() {
-    const { type = 'button' } = this.props
+    const { type = "button", onClick } = this.props;
     return (
-      <button type={type} className={style.botao}>
+      <button onClick={onClick} type={type} className={style.botao}>
         {this.props.texto}
       </button>
-    )
+    );
   }
 }
 
-export default Botao
+export default Botao;
